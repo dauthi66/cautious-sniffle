665b5170466e1a22932c3b1fa393f0a64486c02c
Kris <dauthi66@gmail.com>
2021-10-07 17:22:59 -0700

    added no text validation and id refactoring

--- js/main.js
+++ js/main.js
@@ -3,5 +3,21 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
-    alert("button hit");
+    var fNameErr = "Please enter your first name";
+    isTextPresent("first_name", fNameErr);
+    var lNameErr = "Please enter your last name";
+    isTextPresent("last_name", lNameErr);
+}
+function isTextPresent(id, errMsg) {
+    var txtBox = $(id);
+    var userTxt = txtBox.value;
+    if (userTxt == "") {
+        var errSpan = txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
+}
+function $(id) {
+    return document.getElementById(id);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -4,5 +4,32 @@ window.onload = function() {
 }
 
 function main(): void {
-    alert("button hit");
+    let fNameErr = "Please enter your first name";
+    isTextPresent("first_name", fNameErr);
+
+    let lNameErr = "Please enter your last name";
+    isTextPresent("last_name", lNameErr);
+
+
+}
+/**
+ * Returns true if the user enters text
+ * @param id id of the text to validate
+ * @param errMsg error message given to user
+ * @returns 
+ */
+function isTextPresent(id: string, errMsg:string):boolean {
+    let txtBox = <HTMLInputElement>$(id);
+    let userTxt = txtBox.value;
+
+    if (userTxt == "") {
+        let errSpan = <HTMLSpanElement>txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
+}
+
+function $(id):HTMLElement {
+    return document.getElementById(id);
 }
